345cae430332e4597e3ded2feae4657bbc57f23f
Israel Rodrigues <israel.rodrigues@invillia.com>
2022-02-06 03:00:33 -0300

    Adicionar arquivos de projeto.

---
+++ DesignPatterns/Creational/Abstract Factory/CreateFactory.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignPatterns.Creational.Abstract_Factory
+{
+    public interface ICreateFactory
+    {
+        ICamisa CreateCamisa();
+        IShorts CreateShorts();
+        ITenis CreateTenis();
+    }
+
+    public class Camisa : ICamisa
+    {
+        public string Vestir()
+        {
+            return "Vestir Camisa";
+        }
+    }
+
+    public class Shorts : IShorts
+    {
+        public string Vestir()
+        {
+            return "Vestir Shorts";
+        }
+    }
+
+    public class Tenis : ITenis
+    {
+        public string Vestir()
+        {
+            return "Calçar Tenis";
+        }
+    }
+
+
+    public interface ICamisa
+    {
+        string Vestir();
+    }
+
+    public interface IShorts
+    {
+        string Vestir();
+    }
+
+    public interface ITenis
+    {
+        string Vestir();
+    }
+
+    public class NikeFactory : ICreateFactory
+    {
+        public ICamisa CreateCamisa()
+        {
+            Console.WriteLine("Camisa Nike Criada");
+            return new Camisa();
+        }
+
+        public IShorts CreateShorts()
+        {
+            Console.WriteLine("Shorts Nike Criada");
+            return new Shorts();
+        }
+
+        public ITenis CreateTenis()
+        {
+            Console.WriteLine("Tenis Nike Criada");
+            return new Tenis();
+        }
+    }
+
+    public class AdidasFactory : ICreateFactory
+    {
+        public ICamisa CreateCamisa()
+        {
+            Console.WriteLine("Camisa Adidas Criada");
+            return new Camisa();
+        }
+
+        public IShorts CreateShorts()
+        {
+            Console.WriteLine("Shorts Adidas Criada");
+            return new Shorts();
+        }
+
+        public ITenis CreateTenis()
+        {
+            Console.WriteLine("Tenis Adidas Criada");
+            return new Tenis();
+        }
+    }
+
+    public class Client
+    {
+        public void Main()
+        {
+            Console.WriteLine("Client: Testando client code com NikeFactory");
+            ClientMethod(new NikeFactory());
+
+            Console.WriteLine("------------------------");
+
+            Console.WriteLine("Client: Testando client code com AdidasFactory");
+            ClientMethod(new AdidasFactory());
+        }
+
+        public void ClientMethod(ICreateFactory roupasFactory)
+        {
+            var camisa = roupasFactory.CreateCamisa();
+
+            var shorts = roupasFactory.CreateShorts();
+
+            var tenis = roupasFactory.CreateTenis();
+
+            Console.WriteLine(camisa.Vestir());
+
+            Console.WriteLine(shorts.Vestir());
+
+            Console.WriteLine(tenis.Vestir());
+        }
+    }
+}
---
+++ DesignPatterns/Creational/FactoryMethod/CreatorProduct.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignPatterns.Creational.Factory_Method
+{
+    public abstract class CreatorProduct
+    {
+
+        public abstract IProduct FactoryMethod();
+
+        public string SomeOperation()
+        {
+            var product = FactoryMethod();
+
+            var result = "Creator: Them same creator's code has just worked with "
+                + product.Operation();
+
+            return result;
+        }
+    }
+
+    public class NavioCreator : CreatorProduct
+    {
+        public override IProduct FactoryMethod()
+        {
+            return new ConcretNavio();
+        }
+    }
+
+    public class CaminhaoCreator : CreatorProduct
+    {
+        public override IProduct FactoryMethod()
+        {
+            return new ConcretCaminhao();
+        }
+    }
+
+    public interface IProduct
+    {
+        string Operation();
+    }
+
+    public class ConcretNavio : IProduct
+    {
+        public string Operation()
+        {
+            return "{Result of CONCRET NAVIO}";
+        }
+    }
+    public class ConcretCaminhao : IProduct
+    {
+        public string Operation()
+        {
+            return "{Result of CONCRET CAMINHAO}";
+        }
+    }
+
+    public class Client
+    {
+        public void Main()
+        {
+            Console.WriteLine("App: Rodando o NavioCreator");
+            ClientCode(new NavioCreator());
+
+            Console.WriteLine("----------------------");
+
+            Console.WriteLine("App: Rodando o Caminhao");
+            ClientCode(new CaminhaoCreator());
+        }
+
+
+
+
+        public void ClientCode(CreatorProduct creator)
+        {
+            // ...
+            Console.WriteLine("Client: I'm not aware of the creator's class," +
+                "but it still works.\n" + creator.SomeOperation());
+            // ...
+        }
+    }
+
+}
